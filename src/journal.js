733ca7f320bcbd9f9947dfd9d63e1b8b0b99aa4d
Ethan Bear <ehwbear@icloud.com>
2021-11-08 15:46:14 -0800

    Add vowel counter
    Co-authored-by: Jeff Lai <pjefflai@gmail.com>
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

---
+++ src/journal.js
@@ -0,0 +1,23 @@
+function JournalEntry (title, body) {
+  this.title = title;
+  this.body = body;
+}
+
+JournalEntry.prototype.wordCount = function(){
+  let wordLength = this.body.split(" ");
+  return wordLength.length;
+}
+
+JournalEntry.prototype.getCount = function() {
+  let myObject = {
+    consonants: 0,
+    vowels: 0
+  };
+
+  for(let i = 0, j = this.body.length; i < j; i++) {
+    if('aeiou'.indexOf(this.body[i]) !== -1) myObject.vowels++;
+    if('bcdfghjklmnpqrstvwxyz'.indexOf(this.body[i]) !== -1) myObject.consonants++;
+  }
+
+  return myObject;
+};
